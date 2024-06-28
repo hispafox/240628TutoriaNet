6c4518ee3b33ce048fcb12e98fa83687e6eba432
Pedro Hernandez <hispafox@gmail.com>
2024-06-28 10:58:12 +0200

    Mejoras en autenticación, modelos y Swagger
    Se han realizado cambios significativos en ApiPaises.csproj, WeatherForecastController.cs y Program.cs para mejorar la estructura del proyecto, la autenticación y la documentación de la API. En detalle:
    
    - En ApiPaises.csproj, se añadió una nueva referencia de carpeta "Models\" para organizar mejor los modelos de datos.
    - En WeatherForecastController.cs, se introdujo la directiva `using Microsoft.AspNetCore.Authorization;` y se comentó la anotación `[Authorize]`, preparando el terreno para futuras funcionalidades de autorización.
    - En Program.cs, se mejoró la configuración del servicio y el pipeline de solicitudes HTTP, añadiendo servicios MVC, configuración para Swagger/OpenAPI, y preparando la aplicación para manejar autenticación y autorización de manera más efectiva. Además, se ha hecho un esfuerzo por mejorar la legibilidad y estructura del código mediante comentarios detallados y una organización más clara.
    
    Estos cambios reflejan un avance hacia una aplicación más segura, organizada y fácil de documentar y probar.

--- ApiPaisesSolucion/ApiPaises/Program.cs
+++ ApiPaisesSolucion/ApiPaises/Program.cs
@@ -1,15 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1 - Add services to the container.
+// ==================================
 
+// Agregar el servicio de Modelo Vista Controlador
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Agregar el servicio de base de datos de Entity Framework Core
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// 2 - Configure the HTTP request pipeline.
+// ========================================
+
+// ===========================================
+//   ^           ^
+//   Swagger     ^
+//               MapControllers
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,8 +32,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
---
+++ Ejemplo/ApiPaisesSolucion/ApiPaisesProyecto/Servicios/Saludo.cs
@@ -0,0 +1,24 @@
+﻿namespace ApiPaisesProyecto.Servicios
+{
+    public class Saludo : ISaludo
+    {
+        public string saludar(string mensaje)
+        {
+           return $"Hola {mensaje}";
+        }
+    }
+
+    // Crear una instancia de la interfaz ISaludo pero saludando en inglés
+    public class SaludoEnIngles : ISaludo
+    {
+        public string saludar(string mensaje)
+        {
+            return $"Hello {mensaje}";
+        }
+    }
+
+    public interface ISaludo
+    {
+        string saludar(string mensaje);
+    }
+}
